dfec9a5900923a71514fc8f19da82f1a59212c85
tegajoel <36060039+tegajoel@users.noreply.github.com>
2025-01-28 20:02:29 +0000

    Fixed the button component to allow it have children components. Also made the button onClick and text field optional

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 interface ButtonProps {
-  onClick: () => void;
-  text: string;
+  onClick?: () => void;
+  text?: string;
+  children?: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, text }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, text, children}) => {
   return (
     <div>
       <button onClick={onClick} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
         {text}
+          {children}
       </button>
     </div>
   );
